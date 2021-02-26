67d55387b60cd14c08ab7c9736c65c7a8bd092a4
toriwilliamsch <twilliams@companieshouse.gov.uk>
2021-02-26 07:55:29 +0000

    Removing unneccesary mocking from GlobalExceptionHandlerTest and removing object initialisation in the Oracle DAO test

--- src/test/java/uk/gov/companieshouse/bankruptofficersearch/api/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/uk/gov/companieshouse/bankruptofficersearch/api/exception/GlobalExceptionHandlerTest.java
@@ -3,18 +3,19 @@ package uk.gov.companieshouse.bankruptofficersearch.api.exception;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-@ExtendWith(MockitoExtension.class)
 public class GlobalExceptionHandlerTest {
 
-    @InjectMocks
     private GlobalExceptionHandler globalExceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        this.globalExceptionHandler = new GlobalExceptionHandler();
+    }
 
     @Test
     void testHandleException() {
