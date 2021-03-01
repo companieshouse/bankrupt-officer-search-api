c58f029d90b8c0f446fe8f245be80e3c0e4a8cba
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2021-03-01 16:00:24 +0000

    Add authentication/eric helper

---
+++ src/main/java/uk/gov/companieshouse/bankruptofficersearch/api/util/AuthenticationHelper.java
@@ -0,0 +1,73 @@
+package uk.gov.companieshouse.bankruptofficersearch.api.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.ArrayUtils;
+
+public class AuthenticationHelper {
+
+    private AuthenticationHelper() {
+    }
+
+    public static final String OAUTH2_IDENTITY_TYPE          = "oauth2";
+    public static final String API_KEY_IDENTITY_TYPE         = "key";
+    public static final String ADMIN_SCOTTISH_BANKRUPT_OFFICER_ROLE = "/admin/officer-search/scottish-bankrupt-officer";
+    public static final String API_KEY_ELEVATED_ROLE         = "*";
+
+    private static final String ERIC_IDENTITY             = "ERIC-Identity";
+    private static final String ERIC_IDENTITY_TYPE        = "ERIC-Identity-Type";
+    private static final String ERIC_AUTHORISED_USER      = "ERIC-Authorised-User";
+    private static final String ERIC_AUTHORISED_ROLES     = "ERIC-Authorised-Roles";
+    private static final String ERIC_AUTHORISED_KEY_ROLES = "ERIC-Authorised-Key-Roles";
+
+    public static String getAuthorisedIdentity(HttpServletRequest request) {
+        return getRequestHeader(request, AuthenticationHelper.ERIC_IDENTITY);
+    }
+
+    public static String getAuthorisedIdentityType(HttpServletRequest request) {
+        return getRequestHeader(request, AuthenticationHelper.ERIC_IDENTITY_TYPE);
+    }
+
+    public static String getAuthorisedUser(HttpServletRequest request) {
+        return getRequestHeader(request, AuthenticationHelper.ERIC_AUTHORISED_USER);
+    }
+
+    public static String getAuthorisedRoles(HttpServletRequest request) {
+        return getRequestHeader(request, AuthenticationHelper.ERIC_AUTHORISED_ROLES);
+    }
+
+    public static String[] getAuthorisedRolesArray(HttpServletRequest request) {
+        String roles = getAuthorisedRoles(request);
+        if (roles == null || roles.length() == 0) {
+            return new String[0];
+        }
+
+        // roles are space separated list of authorized roles
+        return roles.split(" ");
+    }
+
+    public static boolean isRoleAuthorised(HttpServletRequest request, String role) {
+        if (role == null || role.length() == 0) {
+            return false;
+        }
+
+        String[] roles = getAuthorisedRolesArray(request);
+        if (roles.length == 0) {
+            return false;
+        }
+
+        return ArrayUtils.contains(roles, role);
+    }
+
+    public static String getAuthorisedKeyRoles(HttpServletRequest request) {
+        return getRequestHeader(request, AuthenticationHelper.ERIC_AUTHORISED_KEY_ROLES);
+    }
+
+    public static boolean isKeyElevatedPrivilegesAuthorised(HttpServletRequest request) {
+        return API_KEY_ELEVATED_ROLE.equals(getAuthorisedKeyRoles(request));
+    }
+
+    private static String getRequestHeader(HttpServletRequest request, String header) {
+        return request == null ? null : request.getHeader(header);
+    }
+}
