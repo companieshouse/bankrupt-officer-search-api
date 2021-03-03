b1ded2a31d5f5c351b966e8c7e989eab7a6d60ae
toriwilliamsch <twilliams@companieshouse.gov.uk>
2021-03-03 15:22:10 +0000

    Adding javadocs to the DAO

--- src/main/java/uk/gov/companieshouse/bankruptofficersearch/api/request/BankruptOfficerDao.java
+++ src/main/java/uk/gov/companieshouse/bankruptofficersearch/api/request/BankruptOfficerDao.java
@@ -6,7 +6,19 @@ import uk.gov.companieshouse.bankruptofficersearch.api.model.response.ScottishBa
 
 public interface BankruptOfficerDao {
 
+    /**
+     * Retrieving Scottish bankrupt officer search results
+     * 
+     * @param search
+     * @return searchResultsEntity - matching Scottish bankrupt officers with pagination data
+     */
     ScottishBankruptOfficerSearchResultsEntity getScottishBankruptOfficers(ScottishBankruptOfficerSearchEntity search);
 
+    /**
+     * Retrieving details for a bankrupt officer
+     *
+     * @param officerId
+     * @return officerDetailsEntity - details of a single Scottish bankrupt officer
+     */
     ScottishBankruptOfficerDetailsEntity getScottishBankruptOfficer(String officerId);
 }
