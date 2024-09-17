3156bc9dac84fbccf8ee580950305ff8cdb98740
gjackson-ch <gjackson@companieshouse.gov.uk>
2024-09-17 10:54:09 +0100

    Correcting heathcheck path

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -9,8 +9,8 @@ locals {
   docker_repo                = "bankrupt-officer-search-api"
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority  = 101
-  lb_listener_paths          = ["/internal/officer-search/scottish-bankrupt-officers*", "/internal/officer-search/scottish-bankrupt-officers/*", "/internal/officer-search/scottish-bankrupt-officers/healthcheck"]
-  healthcheck_path           = "/internal/officer-search/scottish-bankrupt-officers/healthcheck" # healthcheck path for bankrupt-officer-search-api 
+  lb_listener_paths          = ["/internal/officer-search/scottish-bankrupt-officers*", "/internal/officer-search/scottish-bankrupt-officers/*"]
+  healthcheck_path           = "/internal/officer-search/scottish-bankrupt-officers/health" # healthcheck path for bankrupt-officer-search-api 
   healthcheck_matcher        = "200"
   vpc_name                   = local.stack_secrets["vpc_name"]
   s3_config_bucket           = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
